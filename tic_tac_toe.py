40aa5150ce20cd3b2d00c8a66d9c6fcb0b324807
Zsuzsa <zsuzsapethocodecool@gmail.com>
2017-06-29 14:37:47 +0200

    before pep8

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,22 +3,66 @@ from random import randint
 
 def del_win():
     print("\033c")
-def write_label():
+
+def name_inputs():
+    name1 = name('first')
+    name_not_ok = True
+    while name_not_ok:
+        name2 = name('second')
+        if name1 != name2:
+            name_not_ok = False
+        else:
+            print("Names have to be different!")
+    return name1, name2
+
+def table_gen():
+    row_grater = True
+    while row_grater:
+        row = read_num("Please add the size of the table (3-30): ")
+        if row < 30 and row > 2:
+            row_grater = False
+    Matrix = [[" " for x in range(row)] for y in range(row)]
+    print_board(Matrix)
+    return Matrix, row
+
+def comp_steps(Matrix, ch, player, row):
+    AutoGenNum = True
+    while AutoGenNum:
+        target = []
+        target.append(randint(1, row-1))
+        target.append(randint(1, row-1))
+        AutoGenNum = empty_steps(target,Matrix,"")
+    check_step(target, Matrix, ch)
+    print_board(Matrix)
+    gameIsOngoing2 = check_hits(Matrix, target, ch, player)
+    return gameIsOngoing2, target
+
+def write_label(leng):
     print("   ", end = "")
-    for i in range (len(Matrix[0])):
-        print(" " + str(i+1), end = " ")
+    for i in range (leng):
+        if i > 7:
+            print(" " + str(i+1), end = "")
+        else:
+            print(" " + str(i+1), end = " ")
     print('')
 
 
 def print_board (Matrix):
     del_win()
-    write_label()
+    write_label(len(Matrix[0]))
     for i in range (0, len(Matrix[0])):
-        print(" " + str(i+1), end = " ")
+        if i > 8:
+            print(" " + str(i+1), end = "")
+        else:
+            print(" " + str(i+1), end = " ")
         for element in Matrix[i]:
             sys.stdout.write("[{}]".format(*element))
         print(i+1)
-    write_label()
+    write_label(len(Matrix[0]))
+
+def name(who):
+    name = input('Type the name of the {0} player: '.format(who))
+    return name
 
 def read_num(question):
     cycle_bool = True
@@ -31,15 +75,15 @@ def read_num(question):
         cycle_bool = False
     return number
 
-def read_coords():
+def read_coords(player, Matrix, ch):
     my_bool = True
     while my_bool:
         coords = []
-        coords.append(read_num('Add the coordinate \n x: ')-1)
+        coords.append(read_num(player + ', add the coordinate \n x: ')-1)
         coords.append(read_num(' y: ')-1)
         try:
-            if not empty_steps(coords):
-                check_step(coords)
+            if not empty_steps(coords,Matrix,"The location you chose is not empty!!!"):
+                check_step(coords, Matrix, ch)
             else:
                 continue
         except:
@@ -49,69 +93,35 @@ def read_coords():
     print_board(Matrix)
     return coords
 
-
-def player_change():
-    global player
-    global ch
-    if player == 1:
-        player = 2
+def player_change(name1, name2, player, ch):
+    if player == name1:
+        player = name2
         ch = "O"
     else:
-        player = 1
+        player = name1
         ch = "X"
+    return player, ch
 
-
-def empty_steps (choice):
+def empty_steps (choice, Matrix, message):
     if Matrix[choice[0]][choice[1]] != " ":
-        print("The location you chose is not empty!!!")
+        print(message)
         return True
 
-
-def empty_stepsAuto (choice):
-    global Matrix
-    if choice == 1: 
-        if Matrix[2][0] != " ":
-            return True
-    elif choice ==2:
-        if Matrix[2][1] != " ":
-            return True
-    elif choice ==3:
-        if Matrix[2][2] != " ":
-            return True
-    elif choice ==4:
-        if Matrix[1][0] != " ":
-            return True
-    elif choice ==5:
-        if Matrix[1][1] != " ":
-            return True
-    elif choice ==6:
-        if Matrix[1][2] != " ":
-            return True
-    elif choice ==7:
-        if Matrix[0][0] != " ":
-            return True
-    elif choice ==8:
-        if Matrix[0][1] != " ":
-            return True
-    elif choice ==9:
-        if Matrix[0][2] != " ":
-            return True
-
-def check_step (choice):
+def check_step (choice, Matrix, ch):
     Matrix[choice[0]][choice[1]] = ch
-def compare(Ma,a, b, c, d):
+def compare(Ma,a, b, c, d, ch, player):
+    if a<0 or b<0 or c<0 or d<0:
+        return False
     try:
         if Ma[a][b] == ch and Ma[c][d] == ch:
             print(player, end="")
-            print(". player wins!\n")
-            winner = True
-            return winner
+            print(" wins!\n")
+            return True
     except IndexError:
-        pass
-    winner = False
-    return winner
+        return False
+    return False
 
-def check_hits(Ma, ind):
+def check_hits(Ma, ind, ch, player):
     x = ind[0]
     y = ind[1]
     tab = [[x,y+1,x,y-1],[x,y+1,x,y+2],[x,y-1,x,y-2],
@@ -121,38 +131,12 @@ def check_hits(Ma, ind):
     winner = False
     for row in tab:
         if not winner:
-            winner = compare(Ma,row[0],row[1],row[2],row[3])
+            print(row)
+            winner = compare(Ma,row[0],row[1],row[2],row[3], ch, player)
     if winner:
         return False
     else:
-        return table_check(Matrix)
-def check_hits_comp(Matrix):
-    winner = False
-    gameIsOngoing = True
-    for i in range (0, len (Matrix[0])):
-        if Matrix [i][0] == ch and Matrix [i][1] ==ch and Matrix [i][2] == ch:
-            print("\n This really blonde computer could beat you :D\n")
-            winner = True
-            gameIsOngoing = False
-
-    for i in range (0, len (Matrix[0])):
-        if Matrix [0][i] == ch and Matrix [1][i] == ch and Matrix [2][i] == ch:
-            print("\n This really blonde computer could beat you :D\n")
-            winner = True
-            gameIsOngoing = False
-
-    if Matrix [0][0] ==ch and Matrix [1][1] == ch and Matrix [2][2] == ch:
-            print("\n This really blonde computer could beat you :D\n")
-            winner = True
-            gameIsOngoing = False
-
-    if Matrix [0][2] == ch and Matrix [2][0] == ch and Matrix [1][1] == ch:
-            print("\n This really blonde computer could beat you :D\n")
-            winner = True
-            gameIsOngoing = False
-    if winner == False:
-        gameIsOngoing = table_check(Matrix)
-    return gameIsOngoing
+        return table_check(Ma)
 
 def table_check(Matrix):
     spaceHasFound = 0
@@ -167,99 +151,55 @@ def table_check(Matrix):
     return gameIsOngoing
 
 def gameAuto():
-    global Matrix
-    global ch
-    global player
-    global target
-    signs = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-    row = 3
-    Matrix = [[" " for x in range(row)] for y in range(row)]
-    print_board(Matrix)
+    name1 = name('first')
+    name2 = 'Computer'
+    Matrix, row = table_gen()
     gameIsOngoing = True
     gameIsOngoing2 = True
     draw = False
     while gameIsOngoing and gameIsOngoing2:
-        player = 2
+        player = name2
         ch = 'C'
-        AutoGenNum = True
-        while AutoGenNum:
-            target = (randint(1, 9))
-            AutoGenNum = empty_stepsAuto(target)
-        check_step(target)
-        gameIsOngoing2 = check_hits_comp(Matrix)
-        player = 1
+        gameIsOngoing2, target = comp_steps(Matrix, ch, player, row)
+        player = name1
         ch = 'X'
-        myChar = gameIsOngoing2
-        while myChar:
-            try:
-                choice= int(input("Add a number to put your character: "))
-            except (ValueError, UnboundLocalError):
-                q = input("Use the numerical part of your keypad (1-9) or press q if you want to quit")
-                if q == 'q':
-                    sys.exit()
-                else:
-                    continue
-            if choice in signs:
-                myChar = False
-                myChar = empty_steps(choice) 
-            else:
-                print("Oops, undetectible character.")
-        if gameIsOngoing2 == True:
-            check_step(choice)
-            gameIsOngoing = check_hits(Matrix)
+        if gameIsOngoing2:
+            coords = read_coords(player, Matrix, ch)
+            gameIsOngoing = check_hits(Matrix, coords, ch, player)
+
 
 def game():
-    global Matrix
-    global ch
-    global player
-    player = 1
+    name1, name2 = name_inputs()
+    player = name1
     ch = 'X'
-    row = read_num("Please add the size of the table: ")
-    Matrix = [[" " for x in range(row)] for y in range(row)]
-    print_board(Matrix)
+    Matrix, row = table_gen()
     gameIsOngoing = True
     while gameIsOngoing:
-        coords = read_coords()
-        #try:
-        gameIsOngoing = check_hits(Matrix, coords)
-        #except:
-            #print("vmi")    
-        player_change()
+        coords = read_coords(player, Matrix, ch)
+        gameIsOngoing = check_hits(Matrix, coords, ch, player)
+        player, ch = player_change(name1,name2, player, ch) 
 
+def main():
+    try:
+        ansYes = { 'y', 'Yes', 'Y', 'yes'}
+        ansNo = { 'n', 'No', 'N', 'no'}
+        ansAuto = { 'a', 'A', 'auto', 'Auto'}
+        del_win()
+        print("Hello User! This is a simple tic tac toe game. \n\nYou can play alone or in pair. \n\nThe first player play with the character X and the second one with O.\n")
+        answer = True
+        while answer:
+            cho = input("Would you like to play a round? \n    -press a or A or type auto / Auto to play against the computer \n    -press y or Y / type yes or Yes to play\n    -press n or N / type no or No to close the program\n ")
+            if cho in ansYes:
+                game()
+            elif cho in ansNo:
+                sys.exit()
+            elif cho in ansAuto:
+                gameAuto()
+            else:
+                print("Oops, what???.")
+    except KeyboardInterrupt:
+        print("\nYou stopped the program with entering Ctrl+C. Bye!")
 
-ansYes = { 'y', 'Yes', 'Y', 'yes'}
-ansNo = { 'n', 'No', 'N', 'no'}
-ansAuto = { 'a', 'A', 'auto', 'Auto'}
+if __name__ =='__main__':
+    main()
 
-del_win()
-print("Hello User! This is a simple tic tac toe game. \n\nYou can play alone or in pair. \n\nThe first player play with the character X and the second one with O.\n")
-answer = True
-while answer:
-    cho = input("Would you like to play a round? \n    -press a or A or type auto / Auto to play against the computer \n    -press y or Y / type yes or Yes to play\n    -press n or N / type no or No to close the program\n ")
-    if cho in ansYes:
-        game()
-    elif cho in ansNo:
-        sys.exit()
-    elif cho in ansAuto:
-        gameAuto()
-    else:
-        print("Oops, what???.")
-
-#TODO:
-#kezelni ctrl+C kilépést
-"""
-    winner = compare(Ma,ind[0],ind[1]+1,ind[0],ind[1]-1)
-    winner = compare(Ma,ind[0],ind[1]+1,ind[0],ind[1]+2)
-    winner = compare(Ma,ind[0],ind[1]-1,ind[0],ind[1]-2)
-
-    winner = compare(Ma,ind[0]+1,ind[1],ind[0]-1,ind[1])
-    winner = compare(Ma,ind[0]+1,ind[1],ind[0]+2,ind[1])
-    winner = compare(Ma,ind[0]-1,ind[1],ind[0]-2,ind[1])
-
-    winner = compare(Ma,ind[0]-1,ind[1]-1,ind[0]+1,ind[1]+1)
-    winner = compare(Ma,ind[0]-1,ind[1]-1,ind[0]-2,ind[1]-2)
-    winner = compare(Ma,ind[0]+1,ind[1]+1,ind[0]+2,ind[1]+2)
-
-    winner = compare(Ma,ind[0]-1,ind[1]+1,ind[0]+1,ind[1]-1)
-    winner = compare(Ma,ind[0]-1,ind[1]+1,ind[0]-2,ind[1]+2)
-    winner = compare(Ma,ind[0]+1,ind[1]-1,ind[0]+2,ind[1]-2)"""
